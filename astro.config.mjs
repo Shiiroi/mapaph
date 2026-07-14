eaef15810eb9d591204987cb48795e15ba80d8d3
Vince Magwili <vsmagwili@up.edu.ph>
2026-07-15 02:26:58 +0800

    feat: initial designand contents for the main page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mapaph.com',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
 });
+
 
